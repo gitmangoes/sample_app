a484a66d3d7859dbc870be9bee6b3e93b67c1945
gitmangoes <git@mangoes>
2013-02-04 22:38:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '83d105c9a2a9beba96ee69ddc46d0b144e82753574198dd4216987d5256434ed5c91f22d227cceccac26382b79757ea63a717d33a08d9f36f386f1013138ef47'
